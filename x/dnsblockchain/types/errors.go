91c9f3a1adb5fc675325e36b9ae6f14dc85c387d
Sebastián Téllez <speedwayus@gmail.com>
2025-06-23 09:06:55 -0600

    Enforce TLD Approval for Domain Creation in dnsblockchain Module #10

--- x/dnsblockchain/types/errors.go
+++ x/dnsblockchain/types/errors.go
@@ -10,6 +10,8 @@ import (
 var (
 	ErrInvalidSigner       = errors.Register(ModuleName, 1100, "expected gov account as only signer for proposal message")
 	ErrDuplicateDomainName = errors.Register(ModuleName, 1101, "domain name already exists")
-	ErrInvalidTLD          = errors.Register(ModuleName, 1102, "invalid TLD") // <-- NUEVO ERROR
+	ErrInvalidTLD          = errors.Register(ModuleName, 1102, "invalid TLD")
 	ErrTLDReservedByICANN  = errors.Register(ModuleName, 1103, "TLD is reserved by ICANN and cannot be registered")
+	ErrTLDNotPermitted     = errors.Register(ModuleName, 1104, "the TLD of the domain is not permitted for registration")
+	ErrInvalidDomainName   = errors.Register(ModuleName, 1105, "invalid domain name format")
 )
